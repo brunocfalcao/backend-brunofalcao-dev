8ad4fccc84f258883f27e7277bd25c6a9502c242
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-05-22 21:19:28 +0200

    Update BrunoFalcaoDevServiceProvider.php

--- src/BrunoFalcaoDevServiceProvider.php
+++ src/BrunoFalcaoDevServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace BrunoFalcaoDev;
 
-use Illuminate\Support\Facades\Blade;
 use Eduka\Abstracts\Classes\EdukaServiceProvider;
 
 final class BrunoFalcaoDevServiceProvider extends EdukaServiceProvider
@@ -10,8 +9,6 @@ final class BrunoFalcaoDevServiceProvider extends EdukaServiceProvider
     public function boot()
     {
         $this->dir = __DIR__;
-
-        Blade::anonymousComponentPath(__DIR__.'/../resources/views/components');
 
         parent::boot();
     }
