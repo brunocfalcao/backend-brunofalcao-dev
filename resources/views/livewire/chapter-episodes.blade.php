d905bb79e0ea9167063e53f21014380a14706e8d
Admir Zatega <admir.zatega1009@gmail.com>
2024-10-28 20:47:12 +0000

    Firefox episode slide image aspect-ratio fix

--- resources/views/livewire/chapter-episodes.blade.php
+++ resources/views/livewire/chapter-episodes.blade.php
@@ -81,7 +81,7 @@ $difficulty_badge_classes = [
 		@forelse ($episodes as $episode)
 		<div class="w-full md:w-[30rem] mr-4">
 			<a href="{{ route('episode.play', $episode->uuid) }}" class="cursor-pointer w-full bg-white group rounded-2xl overflow-hidden flex relative">
-				<img src="{{ Vite::asset('resources/images/guitar.jpg') }}" class="rounded-l-2xl w-1/3 flex-shrink-1 object-cover group-hover:brightness-105" style="aspect-ratio: 1/1;">
+				<img src="{{ Vite::asset('resources/images/guitar.jpg') }}" class="rounded-l-2xl w-1/3 flex-shrink-0 object-cover group-hover:brightness-105" style="aspect-ratio: 1/1;">
 
 				<div class="px-6 py-10 flex flex-col justify-center">
 					<h2 class="text-lg font-bold text-gray-900 leading-snug">{{$episode->name}}</h2>
